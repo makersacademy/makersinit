0982271c1239bc63361b60ea6c2fafa2874d385b
Sam Morgan <s_morgan@me.com>
2016-01-28 16:19:49 +0000

    Update `scripts/pre-push` load path using __FILE__

--- lib/makersinit.rb
+++ lib/makersinit.rb
@@ -2,10 +2,16 @@ require "makersinit/version"
 
 module Makersinit
   def self.initialize_pre_push_script
-    File.open('./lib/scripts/pre-push') do |f|
+    File.open(script_path) do |f|
       f.read
     end
 
-    IO.copy_stream('./lib/scripts/pre-push', '.git/hooks/pre-push')
+    IO.copy_stream(script_path, '.git/hooks/pre-push')
+  end
+
+  private
+
+  def self.script_path
+    File.join(File.dirname(File.expand_path(__FILE__)), '../scripts/pre-push')
   end
 end
